242c28a0534a5a80fc4ae6754ed0887ad9ec3c77
neto <neto@netobook>
2017-08-24 01:07:18 -0300

    agenda

--- application/controllers/agenda.php
+++ application/controllers/agenda.php
@@ -12,6 +12,46 @@ class Agenda extends CI_Controller {
         $this->auth->checkAuth('agenda');
 
         try {
+            $prefs['template'] = '
+
+        {table_open}<table class="table table-bordered table-hover">{/table_open}
+
+        {heading_row_start}<tr>{/heading_row_start}
+
+        {heading_previous_cell}<th><a href="{previous_url}">&lt;&lt;</a></th>{/heading_previous_cell}
+        {heading_title_cell}<th colspan="{colspan}">{heading}</th>{/heading_title_cell}
+        {heading_next_cell}<th><a href="{next_url}">&gt;&gt;</a></th>{/heading_next_cell}
+
+        {heading_row_end}</tr>{/heading_row_end}
+
+        {week_row_start}<tr style="font-weight: bold;">{/week_row_start}
+        {week_day_cell}<td>{week_day}</td>{/week_day_cell}
+        {week_row_end}</tr>{/week_row_end}
+
+        {cal_row_start}<tr>{/cal_row_start}
+        {cal_cell_start}<td>{/cal_cell_start}
+        {cal_cell_start_today}<td>{/cal_cell_start_today}
+        {cal_cell_start_other}<td class="other-month">{/cal_cell_start_other}
+
+        {cal_cell_content}<a href="{content}">{day}</a>{/cal_cell_content}
+        {cal_cell_content_today}<div class="highlight"><a href="{content}">{day}</a></div>{/cal_cell_content_today}
+
+        {cal_cell_no_content}{day}{/cal_cell_no_content}
+        {cal_cell_no_content_today}<div class="highlight">{day}</div>{/cal_cell_no_content_today}
+
+        {cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+        {cal_cell_other}{day}{/cal_cel_other}
+
+        {cal_cell_end}</td>{/cal_cell_end}
+        {cal_cell_end_today}</td>{/cal_cell_end_today}
+        {cal_cell_end_other}</td>{/cal_cell_end_other}
+        {cal_row_end}</tr>{/cal_row_end}
+
+        {table_close}</table>{/table_close}
+';
+
+            $this->load->library('calendar', $prefs);
 
             $date = $this->input->post("date");
             if (!$date || $date == '') {
@@ -20,11 +60,48 @@ class Agenda extends CI_Controller {
                 } else {
                     $ds = date("Y-m-d");
                 }
-            }else{
+            } else {
                 $date = inverte_data_w_exception($date);
             }
-            
 
+            if ($this->input->post("mes")) {
+                $mes = $this->input->post("mes");
+                $toView["mes"] = $mes;
+            } else {
+                if ($this->session->flashdata("mes")) {
+                    $mes = $this->session->flashdata("mes");
+                    $toView["mes"] = $mes;
+                }
+            }
+            if ($this->input->post("ano")) {
+                $ano = $this->input->post("ano");
+                $toView["ano"] = $ano;
+            } else {
+                if ($this->session->flashdata("ano")) {
+                    $ano = $this->session->flashdata("ano");
+                    $toView["ano"] = $ano;
+                }
+            }
+
+            $data = array(
+                4 => 'http://example.com/news/article/2006/06/03/',
+                6 => 'http://example.com/news/article/2006/06/07/',
+                13 => 'http://example.com/news/article/2006/06/13/',
+                26 => 'http://example.com/news/article/2006/06/26/'
+            );
+
+            if ($mes && $ano) {
+                $cal = $this->calendar->generate($ano, $mes, $data);
+                $toView["cal"] = $cal;
+            }
+
+
+
+
+
+
+            $post = $this->input->post();
+            $toView["post"] = $post;
 
 
 
--- application/views/agenda/agenda_view.php
+++ application/views/agenda/agenda_view.php
@@ -1,36 +1,52 @@
-<div class="container-fluid">
+<div class="container">
     <div class="panel panel-default">
-        <h3 class="h3 text-center">Agenda</h3>
+        <h3 class="h3 text-center">Definir Dias de Atendimento</h3>
         <hr>
         <div class="panel-body">
-            <a href="#">Novo compromisso</a>
-
-
-
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>
-
-            <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-                <div class="modal-dialog modal-lg panel panel-primary" role="document">
-                    <div class="modal-content">
-                        <div class="panel panel-default">
-                            <h3>Titulo</h3>
-                            <div class="panel-body">
-                                Teste 
-                            </div>
-                        </div>
-                    </div>
+            <div class="col-md-3 panel panel-default">
+                <h4>Selecione o ano e mês a definir</h4>
+                <form action="<?= site_url('agenda')?>" method="post">
+                <label class="">Selecione o ano</label>
+                <select name="ano" class="form-control">
+                    <option value="2017" <?php if(isset($ano)&&$ano==2017){echo "selected";}?>>2017</option>
+                    <option value="2018" <?php if(isset($ano)&&$ano==2018){echo "selected";}?>>2018</option>
+                    <option value="2019" <?php if(isset($ano)&&$ano==2019){echo "selected";}?>>2019</option>
+                    <option value="2020" <?php if(isset($ano)&&$ano==2020){echo "selected";}?>>2020</option>
+                    <option value="2021" <?php if(isset($ano)&&$ano==2021){echo "selected";}?>>2021</option>
+                    <option value="2022" <?php if(isset($ano)&&$ano==2022){echo "selected";}?>>2022</option>
+                    <option value="2023" <?php if(isset($ano)&&$ano==2023){echo "selected";}?>>2023</option>
+                    <option value="2024" <?php if(isset($ano)&&$ano==2024){echo "selected";}?>>2024</option>
+                    <option value="2025" <?php if(isset($ano)&&$ano==2025){echo "selected";}?>>2025</option>
+                    <option value="2026" <?php if(isset($ano)&&$ano==2026){echo "selected";}?>>2026</option>
+                    <option value="2027" <?php if(isset($ano)&&$ano==2027){echo "selected";}?>>2027</option>
+                    <option value="2028" <?php if(isset($ano)&&$ano==2028){echo "selected";}?>>2028</option>
+                </select>
+                <label class="">Selecione o mês</label>
+                <select name="mes" class="form-control">
+                    <option value="1" <?php if(isset($mes)&&$mes==1){echo "selected";}?>>Janeiro</option>
+                    <option value="2" <?php if(isset($mes)&&$mes==2){echo "selected";}?>>Fevereiro</option>
+                    <option value="3" <?php if(isset($mes)&&$mes==3){echo "selected";}?>>Março</option>
+                    <option value="4" <?php if(isset($mes)&&$mes==4){echo "selected";}?>>Abril</option>
+                    <option value="5" <?php if(isset($mes)&&$mes==5){echo "selected";}?>>Maio</option>
+                    <option value="6" <?php if(isset($mes)&&$mes==6){echo "selected";}?>>Junho</option>
+                    <option value="7" <?php if(isset($mes)&&$mes==7){echo "selected";}?>>Julho</option>
+                    <option value="8" <?php if(isset($mes)&&$mes==8){echo "selected";}?>>Agosto</option>
+                    <option value="9" <?php if(isset($mes)&&$mes==9){echo "selected";}?>>Setembro</option>
+                    <option value="10" <?php if(isset($mes)&&$mes==10){echo "selected";}?>>Outubro</option>
+                    <option value="11" <?php if(isset($mes)&&$mes==11){echo "selected";}?>>Novembro</option>
+                    <option value="12" <?php if(isset($mes)&&$mes==12){echo "selected";}?>>Dezembro</option>
+                </select>
+                <br>
+                <div class="text-center">
+                <?= btn("Carregar", "info", "sm", "Carregar")?>
                 </div>
+                </form>
             </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3 panel panel-default" style="font-size: 0.8em;">
+                <?=$cal?>
+            </div>
+            
         </div>
     </div>
 </div>
-<script>
-    $('.modal_full').on('shown.bs.modal', function () {
-        $('#myInput').focus();
-    });
-
-
-    $('.modal_compact').on('shown.bs.modal', function () {
-        $('#myInput').focus();
-    });
-</script>
